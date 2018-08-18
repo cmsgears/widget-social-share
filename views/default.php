9ec548575e443ee2cf12f8b210b2a082156b6eef
Lokendra Chouhan <lokendra.chouhan@vulpinecode.com>
2018-08-18 15:08:41 +0530

    updated code

--- views/default.php
+++ views/default.php
@@ -1,6 +1,6 @@
 <ul class="nav">
     <?php if( array_key_exists( 'facebook', $widget->links ) ) { ?>
-       <li> <a title="Share on Facebook" class="share-facebook <?= $widget->iconSet ?>facebook color-facebook " href="<?= $widget->url ?>"></a></li>
+       <li> <a title="Share on Facebook" class="share-facebook <?= $widget->iconSet ?>facebook color-facebook " href="http://www.facebook.com/sharer.php?u=<?= $widget->url ?>" target="_blank"" ></a></li>
     <?php } ?>
     <?php if( array_key_exists( 'twitter', $widget->links ) ) { ?>
        <li> <a title="Share on Twitter" class="<?= $widget->iconSet ?>twitter color-twitter" href="https://twitter.com/share?url=<?= $widget->url ?>" target="_blank"></a></li>
